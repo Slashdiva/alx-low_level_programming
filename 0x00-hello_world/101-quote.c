33c11b2283ce9c4473954e3780f530667ef7f5c5
Slashdiva <maryannezeogu242@gmail.com>
2022-07-02 12:37:01 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,16 @@
-#incude <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
  ** main - Entry point
- ** Return: Always 1 (success)
+ *
+ ** Return: Always 1 (Success)
+ *
  */
 
 int main(void)
 {
 	printf("and that piece of art is useful\" - Dora korpar, 2015-10-19\n");
+
 	return (1);
 }		
