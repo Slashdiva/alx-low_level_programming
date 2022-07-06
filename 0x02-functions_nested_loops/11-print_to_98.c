6140d776f7f9ece152c9096e32e96242caec9cc8
Slashdiva <maryannezeogu242@gmail.com>
2022-07-06 13:49:53 -0700

    a function that prints all natural numbers from n to 98, followed by a new line.

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,50 +1,25 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_times_table - prints times table
- * @n : times table to use
- * Return:void
+ * print_to_98 - print to 98
+ * @n : number to start from
+ * Return:0 or 1
  */
 
-void print_times_table(int n)
+void print_to_98(int n)
 {
-	int a = 0, rep, b;
-
-	if (n < 0 || n > 15)
-		return;
-
-	while (a <= n)
+	while (n < 98)
 	{
-		for (b = 0; b <= n; b++)
-		{
-			rep = a * b;
-			if (b == 0)
-				_putchar('0' + rep);
-			else if (rep < 10)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + rep);
-			}
-			else if (rep < 100)
-			{
-				_putchar(' ');
-				_putchar('0' + rep / 10);
-				_putchar('0' + rep % 10);
-			}
-			else
-			{
-				_putchar('0' + rep / 100);
-				_putchar('0' + (rep - 100) / 10);
-				_putchar('0' + rep % 10);
-			}
-			if (b < n)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-		_putchar('\n');
-		a++;
+		printf("%i, ", n);
+		n++;
 	}
+	while (n > 98)
+	{
+		printf("%i, ", n);
+		n--;
+	}
+
+	printf("98");
+	putchar('\n');
 }
