6d88cf4f3a4063007953086e6a4811c40554d535
Slashdiva <maryannezeogu242@gmail.com>
2022-07-18 14:24:23 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * set_string -  sets the value of a pointer to a char
+ * @s: the value to modify
+ * @to: the value to assign
+ *
+ * Return: nothing
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+
+
+100-set_string.c
+
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to a char
+ *
+ * @s: content is to be copied
+ * @to: string
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
