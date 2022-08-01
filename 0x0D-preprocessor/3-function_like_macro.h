8a2f1b6b8712046ff6f66e92eec89222d4414c6b
Slashdiva <maryannezeogu242@gmail.com>
2022-08-01 10:37:18 -0700

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,6 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
-#define ABS(x) ((x > 0) ? (x) * (1) : (x))
+#ifndef THREE_FUNCTION_LIKE_MACRO_H
+#define THREE_FUNCTION_LIKE_MACRO_H
+
+#define ABS(x) (x > 0 ? (x) : (x) * -1)
+
 #endif
